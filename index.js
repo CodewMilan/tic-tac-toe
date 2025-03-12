1c37aa9879a3e99732bcbfaed6dd0852425c346d
Milan Sampath <codewmilan@gmail.com>
2025-03-12 19:46:22 +0530

    logic update

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const winningPositions =[
     [1,4,7],
     [2,5,8],
     [0,4,8],
-    [2,4,6]
+    [2,4,6],
+    [6,7,8]
 
 ];
 
@@ -21,7 +22,12 @@ function initgame(){
     currentPlayer = "X"
     gameGrid = ["","","","","","","","",""]
     newGameBtn.classList.add("invisible");
+    boxes.forEach((box,index) => {
+        box.innerHTML= "";
+        boxes[index].style.pointerEvents = 'all'
+    })
     gameInfo.innerText = `Current Player - ${currentPlayer}`;
+         
 }
 
 initgame();
@@ -33,3 +39,77 @@ boxes.forEach((box, index) =>{
     })
 
 });
+
+function handleClick(index){
+    if(gameGrid[index] == ""){
+        boxes[index].innerHTML = currentPlayer;
+        gameGrid[index] = currentPlayer;
+        boxes[index].style.pointerEvents = 'none'
+        swapTurn();
+        checkWinner();
+    }
+    
+}
+
+function swapTurn(){
+    if(currentPlayer == "X"){
+        currentPlayer = "O";
+        
+    }
+    else{
+        currentPlayer = "X";
+        
+    }
+    gameInfo.innerHTML = `Current Player - ${currentPlayer}`;
+
+}
+
+function checkWinner(){
+    let answer = ""
+
+    winningPositions.forEach((position) =>{
+        if((gameGrid[position[0]] != "" || gameGrid[position[1]] || gameGrid[position[2]] !== "")
+        && (gameGrid[position[0]] == gameGrid[position[1]]) && (gameGrid[position[1]] === gameGrid[position[2]])){
+            
+            //cheking if winner is x
+            if(gameGrid[position[0]] === "X")
+                answer = 'X';
+            else
+                answer="O";
+
+            boxes.forEach((box)=> {
+                box.style.pointerEvents = 'none';
+            })
+
+            boxes[position[0]].classList.add("bg-black");
+            boxes[position[1]].classList.add("bg-black");
+            boxes[position[2]].classList.add("bg-black");
+            
+
+        }
+
+    });
+
+    if(answer !== ""){
+        newGameBtn.classList.remove("invisible")
+        gameInfo.innerHTML = `Game Winner - ${answer}`;
+        return
+        
+    }
+
+    let fillCount = 0;
+    gameGrid.forEach((box)=> {
+        if(box !== "")
+            fillCount++;
+    });
+
+    
+    
+}
+
+newGameBtn.addEventListener("click", initgame)
+
+
+
+
+// newGameBtn.classList.remove("invisible");
